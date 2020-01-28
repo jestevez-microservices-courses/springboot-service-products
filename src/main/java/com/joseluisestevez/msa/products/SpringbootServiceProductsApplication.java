ad7f094d1560dcd3693828e8eb762961f3cf7b22
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-28 11:32:26 +0100

    Change the port with environment variable to raise more than one instance

--- src/main/java/com/joseluisestevez/msa/products/SpringbootServiceProductsApplication.java
+++ src/main/java/com/joseluisestevez/msa/products/SpringbootServiceProductsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootServiceProductsApplication {
 
 	public static void main(String[] args) {
+		// run with environment -Dserver.port=9001
 		SpringApplication.run(SpringbootServiceProductsApplication.class, args);
 	}
 
