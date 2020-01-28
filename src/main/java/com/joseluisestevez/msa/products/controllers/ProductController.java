683a68afdcd939a799c052c0cab89c75069ac73f
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-28 11:51:28 +0100

    Send the transient port in response to check load balancing

--- src/main/java/com/joseluisestevez/msa/products/controllers/ProductController.java
+++ src/main/java/com/joseluisestevez/msa/products/controllers/ProductController.java
@@ -1,8 +1,10 @@
 package com.joseluisestevez.msa.products.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,14 +17,23 @@ public class ProductController {
 
 	@Autowired
 	private ProductService productService;
-	
+
+	@Autowired
+	private Environment env;
+
 	@GetMapping("/list")
 	public List<Product> list() {
-		return productService.findAll();
+		return productService.findAll().stream().map(product -> {
+			product.setPort(
+					Integer.parseInt(env.getProperty("local.server.port")));
+			return product;
+		}).collect(Collectors.toList());
 	}
-	
+
 	@GetMapping("/view/{id}")
 	public Product view(@PathVariable Long id) {
-		return productService.findById(id);
+		Product product = productService.findById(id);
+		product.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		return product;
 	}
 }
